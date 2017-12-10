f5d4e9b73d7770960a43db86339a89b7c8247349
Marc Nazarian <marc.nazarian@gmail.com>
2017-12-10 23:20:48 +0100

    Refactored assertions using assertJ

--- src/test/MessageShould.java
+++ src/test/MessageShould.java
@@ -5,13 +5,10 @@ import main.messageevent.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import static main.messageevent.MessageDeletedEvent.EVENT_MESSAGE_DELETED;
 import static main.messageevent.MessageQuackedEvent.EVENT_MESSAGE_QUACKED;
 import static main.messageevent.MessageReQuackedEvent.EVENT_MESSAGE_REQUACKED;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageShould {
 
@@ -24,103 +21,103 @@ public class MessageShould {
 
     @Test
     public void raise_quacked_when_quack() {
-        Message message = Message.quack(messageEventStream, "alice","some content");
+        Message message = Message.quack(messageEventStream, "alice", "some content");
 
-        assertContains(messageEventStream, EVENT_MESSAGE_QUACKED);
+        assertHistoryContains(EVENT_MESSAGE_QUACKED);
 
-        assertNotNull(message);
-        assertEquals("alice", message.getAuthor());
-        assertEquals("some content", message.getContent() );
+        assertThat(message).isNotNull();
+        assertThat(message.getAuthor()).isEqualTo("alice");
+        assertThat(message.getContent()).isEqualTo("some content");
     }
 
     @Test
     public void raise_deleted_when_delete() {
-        messageEventStream.add(new MessageQuackedEvent("bob","some other content"));
+        messageEventStream.add(new MessageQuackedEvent("bob", "some other content"));
         Message message = new Message(messageEventStream);
-                
+
         message.delete(messageEventStream, "bob");
 
-        assertContains(messageEventStream, EVENT_MESSAGE_DELETED);
+        assertHistoryContains(EVENT_MESSAGE_DELETED);
     }
 
     @Test
     public void not_raise_deleted_when_already_deleted() {
-        messageEventStream.add(new MessageQuackedEvent("alice","some content"));
+        messageEventStream.add(new MessageQuackedEvent("alice", "some content"));
         messageEventStream.add(new MessageDeletedEvent());
         Message message = new Message(messageEventStream);
 
         message.delete(messageEventStream, "alice");
 
-        assertContainsOnlyOne(messageEventStream, EVENT_MESSAGE_DELETED);
+        assertHistoryContainsOnlyOnce(EVENT_MESSAGE_DELETED);
     }
 
     @Test
     public void not_raise_two_deleted_when_user_delete_twice() {
-        messageEventStream.add(new MessageQuackedEvent("alice","some content"));
+        messageEventStream.add(new MessageQuackedEvent("alice", "some content"));
         Message message = new Message(messageEventStream);
 
         message.delete(messageEventStream, "alice");
         message.delete(messageEventStream, "alice");
 
-        assertContainsOnlyOne(messageEventStream, EVENT_MESSAGE_DELETED);
+        assertHistoryContainsOnlyOnce(EVENT_MESSAGE_DELETED);
     }
 
     @Test
     public void not_quack_a_message_with_empty_content() {
-        Message.quack(messageEventStream, "bob","");
+        Message.quack(messageEventStream, "bob", "");
 
-        assertDoesNotContain(messageEventStream, EVENT_MESSAGE_QUACKED);
+        assertHistoryDoesNotContain(EVENT_MESSAGE_QUACKED);
     }
 
     @Test
     public void not_quack_a_message_that_contains_more_than_42_chars() {
-        Message.quack(messageEventStream, "carl","abcdefghijklmnopqrstuvwxyz abcdefghijklmnop");
+        Message.quack(messageEventStream, "carl", "abcdefghijklmnopqrstuvwxyz abcdefghijklmnop");
 
-        assertDoesNotContain(messageEventStream, EVENT_MESSAGE_QUACKED);
+        assertHistoryDoesNotContain(EVENT_MESSAGE_QUACKED);
     }
 
     @Test
     public void not_quack_a_message_that_contains_f_word() {
-        Message.quack(messageEventStream, "dude","seriously dude!what is this f*cking stuff?");
+        Message.quack(messageEventStream, "dude", "seriously dude!what is this f*cking stuff?");
 
-        assertDoesNotContain(messageEventStream, EVENT_MESSAGE_QUACKED);
+        assertHistoryDoesNotContain(EVENT_MESSAGE_QUACKED);
     }
 
     @Test
     public void not_allow_delete_when_deleter_is_not_message_author() {
-        messageEventStream.add(new MessageQuackedEvent("alice","some content"));
+        messageEventStream.add(new MessageQuackedEvent("alice", "some content"));
         Message message = new Message(messageEventStream);
 
         message.delete(messageEventStream, "bob");
 
-        assertDoesNotContain(messageEventStream, EVENT_MESSAGE_DELETED);
+        assertHistoryDoesNotContain(EVENT_MESSAGE_DELETED);
     }
 
     @Test
     public void raise_re_quacked_when_re_quack() {
-        messageEventStream.add(new MessageQuackedEvent("elena","some awesome content"));
+        messageEventStream.add(new MessageQuackedEvent("elena", "some awesome content"));
         Message message = new Message(messageEventStream);
 
         Message reQuackedMessage = message.reQuack(messageEventStream, "franck");
 
-        assertContains(messageEventStream, EVENT_MESSAGE_REQUACKED);
-        assertEquals("franck", reQuackedMessage.getAuthor());
-        assertEquals("RQ: some awesome content", reQuackedMessage.getContent());
+        assertHistoryContains(EVENT_MESSAGE_REQUACKED);
+        assertThat(reQuackedMessage.getAuthor()).isEqualTo("franck");
+        assertThat(reQuackedMessage.getContent()).isEqualTo("RQ: some awesome content");
     }
 
     @Test
     public void not_allow_re_quack_of_your_own_messages() {
-        messageEventStream.add(new MessageQuackedEvent("god","my ultimate message"));
+        messageEventStream.add(new MessageQuackedEvent("god", "my ultimate message"));
         Message message = new Message(messageEventStream);
 
         message.reQuack(messageEventStream, "god");
 
-        assertDoesNotContain(messageEventStream, EVENT_MESSAGE_REQUACKED);
+        assertHistoryDoesNotContain(EVENT_MESSAGE_REQUACKED);
     }
 
     @Test
     public void increment_the_number_of_re_quacks_and_update_list_of_re_quackers_when_requack() {
-        messageEventStream.add(new MessageQuackedEvent("helen","super inspiring message"));
+        messageEventStream.add(new MessageQuackedEvent("helen", "super inspiring message"));
         messageEventStream.add(new MessageReQuackedEvent("ian"));
         messageEventStream.add(new MessageReQuackedEvent("jane"));
         messageEventStream.add(new MessageReQuackedEvent("ken"));
@@ -128,28 +125,21 @@ public class MessageShould {
 
         message.reQuack(messageEventStream, "laura");
 
-        assertContains(messageEventStream, EVENT_MESSAGE_REQUACKED, 4);
-        assertEquals(4, message.getNumberOfReQuacks());
-        assertEquals(Arrays.asList("ian", "jane", "ken", "laura"), message.getReQuackedBy());
+        assertThat(messageEventStream.getMessageEvents()).extracting("name").containsExactly(EVENT_MESSAGE_QUACKED, EVENT_MESSAGE_REQUACKED, EVENT_MESSAGE_REQUACKED, EVENT_MESSAGE_REQUACKED, EVENT_MESSAGE_REQUACKED);
+        assertThat(message.getNumberOfReQuacks()).isEqualTo(4);
+        assertThat(message.getReQuackedBy()).containsExactly("ian", "jane", "ken", "laura");
     }
 
-    private static void assertContains(MessageEventStream messageEventStream, String eventName) {
-        assertTrue(getNumberOfEventsNamed(messageEventStream.getMessageEvents(), eventName) >= 1);
+    private void assertHistoryContains(String eventName) {
+        assertThat(messageEventStream.getMessageEvents()).extracting("name").contains(eventName);
     }
 
-    private void assertContainsOnlyOne(MessageEventStream messageEventStream, String eventName) {
-        assertContains(messageEventStream, eventName, 1);
+    private void assertHistoryDoesNotContain(String eventName) {
+        assertThat(messageEventStream.getMessageEvents()).extracting("name").doesNotContain(eventName);
     }
 
-    private static void assertContains(MessageEventStream messageEventStream, String eventName, int nbOccurrences) {
-        assertEquals(nbOccurrences, getNumberOfEventsNamed(messageEventStream.getMessageEvents(), eventName));
+    private void assertHistoryContainsOnlyOnce(String eventName) {
+        assertThat(messageEventStream.getMessageEvents()).extracting("name").containsOnlyOnce(eventName);
     }
 
-    private static void assertDoesNotContain(MessageEventStream messageEventStream, String eventName) {
-        assertEquals(0, getNumberOfEventsNamed(messageEventStream.getMessageEvents(), eventName));
-    }
-
-    private static long getNumberOfEventsNamed(Collection<MessageEvent> messageEvents, String eventName) {
-        return messageEvents.stream().filter(e->e.getName().equals(eventName)).count();
-    }
 }
